d35ede95e9be97dc3bec857a4666c6d2bd493b61
MiaomiaoShi1004 <simoneshi1004@outlook.com>
2023-12-08 14:08:55 +0000

    Enble editing current item and delete item functionality.

--- ToDoList/ViewController.swift
+++ ToDoList/ViewController.swift
@@ -12,25 +12,46 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     // context is an instance of "NSManagedObjectContext", a core part of the COre Data Stack. It manages a collection of "NSManagedObject" instances(which are data records that you fetch, insert, delete, or modify)
     let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
     
+    // Creating a UITableView programmatically and setting up its basic properties.
     let tableView: UITableView = {
         let table = UITableView()
         table.register(UITableViewCell.self,
         forCellReuseIdentifier: "cell")
         
+        // The table is returned and can be used in this view controller
         return table
     }()
     
-    // ???
+    // This array will hold the ToDoListItem objects fetched from Core Data.
     private var models = [ToDoListItem]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        title = "CoreData To Do List"
+        title = "To Do List"
         view.addSubview(tableView)
+        getAllitems()
         tableView.delegate = self
         tableView.dataSource = self
         tableView.frame = view.bounds
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(didTapAdd))
+    }
+    
+    @objc private func didTapAdd(){
+        let alert = UIAlertController(title: "New Item", message: "Enter new item", preferredStyle: .alert)
+        
+        alert.addTextField(configurationHandler: nil)
+        // what does weak self do here? what's the relation between this weak self and the self?.createItem?
+        alert.addAction(UIAlertAction(title: "Submit", style: .cancel, handler: { [weak self] _ in
+            guard let field = alert.textFields?.first, let text = field.text, !text.isEmpty else {
+                return
+            }
+            // what is self here?
+            self?.createItem(name: text)
+        }))
+        
+        present(alert, animated: true)
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -41,8 +62,43 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         let model = models[indexPath.row]
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
         cell.textLabel?.text = model.name
+       
         return cell
     }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+        let item = models[indexPath.row]
+        
+        let sheet = UIAlertController(title: "Edit Panel", message: nil, preferredStyle: .actionSheet)
+        
+        sheet.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+        
+        sheet.addAction(UIAlertAction(title: "Edit", style: .default, handler: { [weak self] _ in
+            let alert = UIAlertController(title: "Edit Item", message: "Edit your item", preferredStyle: .alert)
+            
+            alert.addTextField(configurationHandler: nil)
+            alert.textFields?.first?.text = item.name
+            alert.addAction(UIAlertAction(title: "Save", style: .cancel, handler: { [weak self] _ in
+                guard let field = alert.textFields?.first, let newName = field.text, !newName.isEmpty else {
+                    return
+                }
+                // what is self here?
+                self?.updateItem(item: item, newName: newName)
+            }))
+            
+            self?.present(alert, animated: true)
+        }))
+        
+        sheet.addAction(UIAlertAction(title: "Delete", style: .destructive, handler: { [weak self] _ in
+            self?.deleteItem(item: item)
+        }))
+
+
+        present(sheet, animated: true)
+    }
+    
     
     // Core Data
     func getAllitems() {
@@ -66,6 +122,8 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         // save the changes in the databse
         do {
             try context.save()
+            //reload our table view
+            getAllitems()
         } catch {
             
         }
@@ -77,6 +135,7 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         
         do {
             try context.save()
+            getAllitems()
         } catch {
             
         }
@@ -87,6 +146,7 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         
         do {
             try context.save()
+            getAllitems()
         } catch {
             
         }
